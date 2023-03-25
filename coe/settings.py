be6422efc0e97d92c5cb1ba7d29e74553b61ac00
nisarga3 <ms.nisarga3@gmail.com>
2023-03-25 22:58:28 +0530

    staticfiles

--- coe/settings.py
+++ coe/settings.py
@@ -139,7 +139,11 @@ COMPRESS_ROOT = BASE_DIR / 'static'
 
 COMPRESS_ENABLED = True
 
-STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
-
+# STATICFILES_FINDERS = ('compressor.finders.CompressorFinder','')
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
 STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles/")
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static/')]
